8f0b0af08b4b786ca1fecbeb4efb30de29e358d6
Sarvesh <sarveshshahane2004@gmail.com>
2023-10-20 14:33:29 +0530

    refactor last commited code using asyncHandler

--- backend/controllers/canteenController.js
+++ backend/controllers/canteenController.js
@@ -1,85 +1,71 @@
+const asyncHandler = require('express-async-handler');
 const Breakfast = require('../models/breakfastModel');
 const Lunch = require('../models/lunchModel');
 const Dinner = require('../models/dinnerModel');
 
 // Controller function to get the canteen's dashboard
-const getCanteenDashboard = async (req, res) => {
+const getCanteenDashboard = asyncHandler(async (req, res, next) => {
   const canteenId = req.params.id;
 
-  try {
-    const breakfastDishes = await Breakfast.find({ canteenId });
-    const lunchDishes = await Lunch.find({ canteenId });
-    const dinnerDishes = await Dinner.find({ canteenId });
+  const breakfastDishes = await Breakfast.find({ canteenId }).exec();
+  const lunchDishes = await Lunch.find({ canteenId }).exec();
+  const dinnerDishes = await Dinner.find({ canteenId }).exec();
 
-    res.json({
-      breakfast: breakfastDishes.length > 0 ? breakfastDishes : ["Not Added Yet"],
-      lunch: lunchDishes.length > 0 ? lunchDishes : ["Not Added Yet"],
-      dinner: dinnerDishes.length > 0 ? dinnerDishes : ["Not Added Yet"],
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
+  res.json({
+    breakfast: breakfastDishes.length > 0 ? breakfastDishes : ['Not Added Yet'],
+    lunch: lunchDishes.length > 0 ? lunchDishes : ['Not Added Yet'],
+    dinner: dinnerDishes.length > 0 ? dinnerDishes : ['Not Added Yet'],
+  });
+});
 
 // Controller function to add a breakfast dish
-const addBreakfastDish = async (req, res) => {
+const addBreakfastDish = asyncHandler(async (req, res, next) => {
   const canteenId = req.params.id;
   const { dish } = req.body;
 
-  try {
-    const existingDish = await Breakfast.findOne({ canteenId, dish });
+  const existingDish = await Breakfast.findOne({ canteenId, dish }).exec();
 
-    if (existingDish) {
-      return res.json({ message: 'Dish already added' });
-    }
-
-    const newDish = new Breakfast({ canteenId, dish });
-    await newDish.save();
-
-    res.json({ message: 'Dish added successfully' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal server error' });
+  if (existingDish) {
+    return res.json({ message: 'Dish already added' });
   }
-};
+
+  const newDish = new Breakfast({ canteenId, dish });
+  await newDish.save();
+
+  res.json({ message: 'Dish added successfully' });
+});
 
 // Controller function to remove a breakfast dish
-const removeBreakfastDish = async (req, res) => {
+const removeBreakfastDish = asyncHandler(async (req, res, next) => {
   const canteenId = req.params.id;
   const { dish } = req.body;
 
-  try {
-    await Breakfast.deleteOne({ canteenId, dish });
-    res.json({ message: 'Dish removed successfully' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
+  await Breakfast.deleteOne({ canteenId, dish }).exec();
+  res.json({ message: 'Dish removed successfully' });
+});
 
 // Implement similar functions for lunch and dinner dishes
 // addLunchDish, removeLunchDish, addDinnerDish, and removeDinnerDish
 
 // Controller function to add a lunch dish
-const addLunchDish = async (req, res) => {
+const addLunchDish = asyncHandler(async (req, res, next) => {
   // Similar to addBreakfastDish
-};
+});
 
 // Controller function to remove a lunch dish
-const removeLunchDish = async (req, res) => {
+const removeLunchDish = asyncHandler(async (req, res, next) => {
   // Similar to removeBreakfastDish
-};
+});
 
 // Controller function to add a dinner dish
-const addDinnerDish = async (req, res) => {
+const addDinnerDish = asyncHandler(async (req, res, next) => {
   // Similar to addBreakfastDish
-};
+});
 
 // Controller function to remove a dinner dish
-const removeDinnerDish = async (req, res) => {
+const removeDinnerDish = asyncHandler(async (req, res, next) => {
   // Similar to removeBreakfastDish
-};
+});
 
 module.exports = {
   getCanteenDashboard,
