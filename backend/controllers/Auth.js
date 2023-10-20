9c8a0866362096a85ba0e9d9abc32321dc47f249
Sarvesh <sarveshshahane2004@gmail.com>
2023-10-21 01:07:04 +0530

    update auth.js

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const User = require("../models/User");
+const User = require("../models/studentLoginInfo");
 const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
